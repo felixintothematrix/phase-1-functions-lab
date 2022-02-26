3af6296efbeb2fcc00633f45d166100e9cf5b887
Felix Garcia <felixgarciarattler@gmail.com>
2022-02-26 11:25:37 -0600

    commit tree 6fa41dc5144a5f325a2a1895845375cffc6b48e1

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+// function distanceFromHqInblocks(start){
+//   return Math.abs(start - 42)
+// };
+  
+// function distanceTravelledInFeet(start, destination){
+//    return Math.abs(destination - start) * 264
+// }
+
+
+function distanceFromHqInBlocks(pickUpLocation){
+   return Math.abs (pickUpLocation - 42)
+
+}
